8a6922c2bbb50b6b65413ff0411054782925d5ac
Reeka <reekamaharaj@gmail.com>
2020-04-22 19:13:07 -0500

    added cat profiles, added form with questions. need to do the back end coding so it works.

--- CatFinder/app/data/cats.js
+++ CatFinder/app/data/cats.js
@@ -1,8 +1,24 @@
 //Save data on cats in this file as an array of objects
 
 {
-    "name": "Jackie",
-    "photo": "",
+    "name": "Journey",
+    "photo": "https://static.wixstatic.com/media/484228_0c814129b64c4a039d1662552bf8e064~mv2.jpg/v1/fill/w_456,h_481,al_c,q_80,usm_0.66_1.00_0.01/journey%20portrait%202.webp",
+    "scores": [
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+    ]
+},
+{
+    "name": "Oran",
+    "photo": "https://static.wixstatic.com/media/484228_4f327da2010b4891ae27c682dc668307~mv2.jpg/v1/fill/w_455,h_597,al_c,q_80,usm_0.66_1.00_0.01/0417oran.webp",
     "scores": [
         5,
         5,
@@ -11,8 +27,182 @@
         5,
         5,
         5,
-        5,
-        5,
-        5
+        3,
+        3,
+        3
     ]
-};
+},
+{
+    "name": "Vienna",
+    "photo": "https://static.wixstatic.com/media/484228_70af5213ffc6446998afaa90c12bcb04~mv2.jpg/v1/fill/w_453,h_338,al_c,q_80,usm_0.66_1.00_0.01/0417vienna.webp",
+    "scores": [
+        3,
+        5,
+        3,
+        1,
+        2,
+        4,
+        4,
+        3,
+        5,
+        3
+    ]
+},
+{
+    "name": "Elton",
+    "photo": "https://static.wixstatic.com/media/484228_3df5c10ece3540d6b2144bbf6a865cc4~mv2.jpg/v1/fill/w_368,h_664,al_c,q_80/elton.webp",
+    "scores": [
+        3,
+        3,
+        2,
+        2,
+        4,
+        4,
+        4,
+        3,
+        5,
+        3
+    ]
+},
+{
+    "name": "Spitzer",
+    "photo": "https://static.wixstatic.com/media/484228_f80348286aa2487289f0df735ed04c17~mv2.jpg/v1/crop/x_0,y_100,w_368,h_466/fill/w_368,h_466,al_c,q_80/sister.webp",
+    "scores": [
+        1,
+        5,
+        1,
+        1,
+        1,
+        2,
+        1,
+        3,
+        5,
+        3
+    ]
+},
+{
+    "name": "Oliver",
+    "photo": "https://static.wixstatic.com/media/484228_2845d1354c3d4843b3a9a49aaf4047f1~mv2.jpg/v1/fill/w_499,h_280,al_c,q_80,usm_0.66_1.00_0.01/Oliver%20rescued.webp",
+    "scores": [
+        1,
+        1,
+        4,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+    ]
+},
+{
+    "name": "Argyle",
+    "photo": "https://static.wixstatic.com/media/484228_f5321facabfb4d58afd33aaa507afbee~mv2.jpg/v1/fill/w_535,h_301,al_c,q_80,usm_0.66_1.00_0.01/0328%20portrait.webp",
+    "scores": [
+        3,
+        1,
+        5,
+        4,
+        5,
+        5,
+        3,
+        3,
+        5,
+        3
+    ]
+},
+{
+    "name": "Dr Fauci",
+    "photo": "https://static.wixstatic.com/media/484228_8b93225e80904e589a8f49ba74162928~mv2.jpg/v1/fill/w_326,h_434,al_c,q_80,usm_0.66_1.00_0.01/0417.webp",
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        4,
+        4,
+        3,
+        3,
+        3,
+        3
+    ]
+},
+{
+    "name": "Mason",
+    "photo": "https://static.wixstatic.com/media/484228_9a0485fb02404bc5891a415e9e31c097~mv2.jpg/v1/fill/w_397,h_517,al_c,q_80/mason%20maddie%202.webp",
+    "scores": [
+        5,
+        4,
+        3,
+        4,
+        4,
+        4,
+        3,
+        3,
+        3,
+        3
+    ]
+},
+{
+    "name": "Billie",
+    "photo": "https://static.wixstatic.com/media/484228_5f87e3e2fc9c4db1a7f88a8851b975c0~mv2.jpg/v1/fill/w_449,h_336,al_c,q_80,usm_0.66_1.00_0.01/billie%202.webp",
+    "scores": [
+        5,
+        3,
+        3,
+        4,
+        4,
+        4,
+        4,
+        4,
+        3,
+        3
+    ]
+},
+{
+    "name": "Maddie",
+    "photo": "https://static.wixstatic.com/media/484228_cbfdb24864ce4471b328c4c883f18469~mv2.jpg/v1/fill/w_431,h_517,al_c,q_80,usm_0.66_1.00_0.01/maddie%20mason%201.webp",
+    "scores": [
+        5,
+        4,
+        3,
+        4,
+        4,
+        4,
+        3,
+        3,
+        3,
+        3
+    ]
+}
+// {
+//     "name": "",
+//     "photo": "",
+//     "scores": [
+//         5,
+//         5,
+//         5,
+//         5,
+//         5,
+//         5,
+//         5,
+//         5,
+//         5,
+//         5
+//     ]
+// },
+
+// Questions
+// - 1 You are looking for a cat that does well with children.
+// - 2 You are looking for a cat that will get along with your cat(s).
+// - 3 You are looking for a cat that will get along with your dog(s)
+// - 4 You are looking for a cat that will be friendly with visitors.
+// - 5 You are looking for a social cat. One that likes to be where the people are.
+// - 6 You are looking for a cat that enjoys pets.
+// - 7 You are looking for a lapcat.
+// - 8 You are looking for a playful cat.
+// - 9 You are looking for a quiet cat.
+// - 10 You are looking for a cat that is vocal.
+
+// https://www.flyandlove.yoga/adoptable-cats
--- README.md
+++ README.md
@@ -18,9 +18,7 @@ Catster hopes to help you find your fur companion. Answer a few questions and we
 ## Role in Development
 
 -------Instructions for homework
-- [Demo](https://friend-finder-fsf.herokuapp.com/)
-- Survey has 10 questions
-    - 1-5 scale for agrees to disagrees
+
 - Scores to find most compatible
     - Convert user scores into an array of numbers.
     - Compare the difference between the user and the cat. Difference between each question. Add up the differences into a totalDifference value, use the absolute value. Do this for all cats in the file
@@ -29,11 +27,11 @@ Catster hopes to help you find your fur companion. Answer a few questions and we
         - Show the name and picture of the closest match
     - Deploy to Heroku
 
-[ ] create the html pages
-[ ] home.html
-[ ] survey.html
-[ ] create the survey questions
-[ ] create the adoptable cat profiles, with questions answered and pictures
+[x] create the html pages
+[x] home.html
+[x] survey.html
+[x] create the survey questions
+[x] create the adoptable cat profiles, with questions answered and pictures
 [ ] code the logic
 [ ] deploy to heroku
 
