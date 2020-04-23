afb5f1551845b795b017d876e17fe498a6b22802
Reeka <reekamaharaj@gmail.com>
2020-04-22 20:41:02 -0500

    server.js setup started

--- CatFinder/app/server.js
+++ CatFinder/app/server.js
@@ -1,1 +1,18 @@
 //require express and path npm packages
+
+//Dependencies
+const express = require("express");
+const path = require("path");
+
+//Express App set up
+const app = express();
+const PORT = 3000;
+
+//Express app set up to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Data
+const cats = require("./data/cats");
+
+//Routes 
