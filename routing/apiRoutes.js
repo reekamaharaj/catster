5334ab1dc0db1feadbbdf58496c50b6283451236
Reeka <reekamaharaj@gmail.com>
2020-05-22 19:22:27 -0500

    got it working and results are displayed AHHHH

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -17,10 +17,6 @@ module.exports = function(app){
         res.json(personData);
     });
 
-    app.get("/api/match", function(_req,res){
-        res.json(purrfectCatData);
-    });
-
     app.get("/", (req, res) => {
         res.render("home");
     });
@@ -50,7 +46,7 @@ module.exports = function(app){
 
         personData.push(newPerson);
         match(newPerson.scores, catData);
-        res.render('results');
+        res.render("results", purrfectCat);
         }
     );
 
@@ -62,47 +58,31 @@ module.exports = function(app){
 function match(person, cats){
     let diff = [ ];
     let lowestNum = 0;
+    
     for(let i = 0; i < cats.length - 1; i++){
         let totalDiff = 0;
         //for each cat where i is the index for the single cat data
         for (let y = 0; y < cats[i].scores.length - 1; y++){
             //get the scores in cat at i, and loop over each where y is the index for the current score
-            // console.log(cats[i].scores[y]);
-            //this does give a number
-
             let compare = Math.abs(cats[i].scores[y] - person[y]);
             totalDiff = totalDiff + compare;
         }
         diff.push(totalDiff);
         
         lowestNum = Math.min(...diff);
+        }
         let indices = findIndex(diff, lowestNum);
-
-        if (indices.length > 1){
-            let purrfectCat = [ ];
-            for (let i = 0; i < indices.length; i++){
-                //get the information for other cats.
-                let cat = JSON.stringify(cats[indices[i]]);
-                purrfectCat.push(cat);
-            }
-            //modal with multiple cats info
-            purrfectCatData.push(purrfectCat);
-        }
-        else {
-            purrfectCat = cats[indices];
-            //modal with the matched cats info
-            purrfectCatData.push(purrfectCat);
-        }
-    }
-    
+        purrfectCat = cats[indices];
 }
 
 function findIndex(array, element){
     let indices = [ ];
+    console.log("diff: " + array + " element: " + element);
     let index = array.indexOf(element);
     while (index !=-1){
         indices.push(index);
         index = array.indexOf(element, index + 1);
     }
+    console.log("Index is: " + indices);
     return indices;
 }
